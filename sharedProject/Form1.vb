4b2d7d8b285fe8eecda2e7911eb91cfe3710cf9e
defcon3 <defcon3@live.com>
2016-08-22 22:14:51 +0200

    Add project files.

---
+++ sharedProject/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(" leck mich am arsch ")
+    End Sub
+
+    ''' <summary>
+    ''' Der Knopf löst eine MessageBox aus.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("lalü-lala")
+    End Sub
+End Class
