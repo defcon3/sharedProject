7a1e196edf0a79fb698fde86bac722d2d7126c99
defcon3 <defcon3@live.com>
2016-08-28 22:19:27 +0200

    neuer Button

--- sharedProject/Form1.vb
+++ sharedProject/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("wo ist der button?")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("hier isser")
+    End Sub
 End Class
