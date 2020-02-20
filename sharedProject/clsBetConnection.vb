137047404ec09d6e2351d3db6d02d96a0e78929e
Veit Luther <defcon3@live.com>
2020-02-20 07:15:28 +0100

    h

--- sharedProject/clsBetConnection.vb
+++ sharedProject/clsBetConnection.vb
@@ -2,13 +2,13 @@
     Inherits clsConnectionRoot
     Public Sub New(jsonstring As String)
 
-
+        MyBase.New(1)
 
         Dim myURI As New Uri(My.Settings.me_betting_uri)
         Dim mySP As System.Net.ServicePoint = System.Net.ServicePointManager.FindServicePoint(myURI)
         mySP.Expect100Continue = False
         webReq = System.Net.WebRequest.Create(myURI)
-        webReq.Method = "POST"
+        'webReq.Method = "POST"
 
 
         Dim byteArray As Byte() = System.Text.Encoding.Default.GetBytes(jsonstring)
@@ -26,6 +26,12 @@
 
 
         '{"method":"SportsAPING/v1.0/listMarketCatalogue","params":{"filter":{"eventTypeIds":[],"marketCountries":[],"marketTypeCodes":[],"marketStartTime":{"from":null,"to":null},"eventIds":[]},"sort":"FIRST_TO_START","maxResults":"20","marketProjection":[]},"jsonrpc":"2.0","id":1}
+
+        ''{@method@:@SportsAPING/v1.0/listMarketCatalogue@,@params@:{@filter@:{@eventTypeIds@:[],@marketCountries@:[],@marketTypeCodes@:[],@marketStartTime@:{@from@:null,@to@:null},@eventIds@:[]},@sort@:@FIRST_TO_START@,@maxResults@:@20@,@marketProjection@:[]},@jsonrpc@:@2.0@,@id@:1}
+
+
+
+
     End Sub
 
 End Class
--- sharedProject/clsConnectionRoot.vb
+++ sharedProject/clsConnectionRoot.vb
@@ -28,15 +28,50 @@ Public MustInherit Class clsConnectionRoot
 
     End Sub
 
+
+    Public Sub New(enu As enumRequest)
+
+        webHeaderColl = New WebHeaderCollection
+
+        webReq = System.Net.WebRequest.Create(get_request_type(enu))
+
+        webHeaderColl.Add("X-Application", CStr(My.Settings.me_delayKey))
+        webHeaderColl.Add("X-Authentication", CStr(My.Settings.me_cookie_ABE))
+        webReq.Method = "POST"
+        webReq.ContentType = "application/json"
+
+    End Sub
+
+
+
+
     ''' <summary>
     ''' Enumeration des Requestes
     ''' </summary>
     Public Enum enumRequest
 
-        betting = CStr(My.Settings.me_delayKey)
-        'me_betting_uri
+        betting = 0
+        heartbeat = 1
+        keepalive = 2
 
     End Enum
+
+    Public Function get_request_type(enu As enumRequest) As String
+
+        Dim returnstring As System.String = ""
+
+        Select Case enu
+            Case = 0 ' betting
+                returnstring = My.Settings.me_betting_uri
+            Case = 1
+                returnstring = My.Settings.me_heartbeat_uri
+            Case = 2
+                returnstring = My.Settings.me_keepAlive_uri
+        End Select
+
+        Return returnstring
+
+    End Function
 
 
 End Class
